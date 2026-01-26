916bba778e9cc31f37b821254328fa764421de2d
sadbutcutee <badcode3@yandex.ru>
2026-01-26 19:29:06 +0300

    feat(core, ui): add login scene and humanized WKWebView logic

---
+++ PinImages/Constants.swift
@@ -0,0 +1,12 @@
+//
+//  Constants.swift
+//  PinImages
+//
+
+enum LoginConstants {
+    static let accessKey = "jAyxCIFb7WuEpdZ-Ot6lM5dYGbNqgrByM2ZUdqbgJPw"
+    static let secretKey = "ieeL1mfqyyUp6QGN79XTef9yPemNjgqDmuRqJPz8e1E"
+    static let redirectURL = "urn:ietf:wg:oauth:2.0:oob"
+    static let accessScope = "public+read_user+write_likes"
+    static let defaultBaseURLString = "https://api.unsplash.com"
+}
