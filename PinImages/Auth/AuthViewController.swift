8cdeef5f4278ce2856670a09757d99fe4d08d7e9
sadbutcutee <badcode3@yandex.ru>
2026-02-09 19:42:49 +0300

    feat(ui): add progressHUD

--- PinImages/Auth/AuthViewController.swift
+++ PinImages/Auth/AuthViewController.swift
@@ -4,6 +4,7 @@
 //
 
 import UIKit
+import ProgressHUD
 
 protocol AuthViewControllerDelegate: AnyObject {
     func didAuthenticate(_ vc: AuthViewController)
@@ -55,8 +56,12 @@ final class AuthViewController: UIViewController {
 extension AuthViewController: WebViewViewControllerDelegate {
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
         
+        ProgressHUD.animate()
+        
         oauth2Service.fetchOAuthToken(code: code) { [weak self] result in
             guard let self = self else { return }
+            
+            ProgressHUD.dismiss()
             
             switch result {
             case .success(let token):
